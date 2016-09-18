f5bc5900b7783619ade8c84bbcbcaa57d618647d
Eric Rogers <eric.j.rogers18@gmail.com>
2016-09-18 11:01:07 -0400

    Initial PHP

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost:3000');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', 'x11c8800');
+   define('DB_DATABASE', 'degree_me');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
