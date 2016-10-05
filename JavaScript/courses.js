b057ad24f2d7b4ad82b1558e474c55d8ef368281
Eric Rogers <ericjrogers18@gmail.com>
2016-10-05 14:10:56 -0400

    added dataTable

--- JavaScript/courses.js
+++ JavaScript/courses.js
@@ -7,18 +7,16 @@ function loadCourses() {
     data: {},
     success: function(json){
       if(json.length != 0) {
-        for(var i = 0; i < json.length; i++) {
-          var table = document.getElementById('course_table');
-          var row = table.insertRow();
-          var courseNum = row.insertCell(0);
-          var courseName = row.insertCell(1);
-          var coursePre = row.insertCell(2);
-          var courseConc = row.insertCell(3);
-          courseNum.innerHTML = json[i].course_number;
-          courseName.innerHTML = json[i].course_name;
-          coursePre.innerHTML = "";
-          courseConc.innerHTML = "";
-        }
+        $('#course_table').DataTable( {
+          paging: false,
+	        data: json,
+	        columns: [
+	            { data: 'course_number'},
+	            { data: 'course_name' },
+	            { data: 'prereq_id' },
+	            { data: 'concentration' }
+	        ],
+		    });
       }
     },
     error:  function() {
