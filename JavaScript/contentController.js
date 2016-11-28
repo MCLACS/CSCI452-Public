d36bc2d3821ed4d7abcea301bb8561791f7b7208
Eric Rogers <ericjrogers18@gmail.com>
2016-11-28 10:19:57 -0500

    finished draft view for adding courses ect.

---
+++ JavaScript/contentController.js
@@ -0,0 +1,15 @@
+var element = "";
+
+function displayFields() {
+  $(element).hide();
+  element = "#" + $('#action').val().toLowerCase() + "-" + $('#type').val().toLowerCase();
+  $(element).show();
+  console.log(element);
+};
+
+function init() {
+  $('#action').change(displayFields);
+  $('#type').change(displayFields);
+};
+
+$(document).ready(init);
