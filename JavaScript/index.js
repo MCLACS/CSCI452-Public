770904fa3433b1f864549bddca9d7728803549c0
Sean-Kelleher <therealseankelleher@gmail.com>
2016-12-03 13:13:43 -0500

    Enter key logs you in

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -72,12 +72,20 @@ function edit(){
   window.location.assign('/CSCI452-Public/Views/editAccount.html');
 }
 
+function enterLog(e){
+  if(e== 13)
+      login();
+}
+
 function init() {
   checkLogin();
   $('#login-button').click(login);
   $('#logout-button').click(logout);
   $('#edit-button').click(edit);
   $('#csCourses').hide();
+  $('.loginField').on("keypress", function(event){
+    enterLog(event.which);
+})
 }
 
 $(document).ready(init);
