191ec106dce6a2b1029ba1eeac9533f10c9824c1
jet98 <jd3437@mcla.edu>
2016-12-05 14:53:53 -0500

    fixed drop down filter

--- PHP/courses.php
+++ PHP/courses.php
@@ -41,11 +41,10 @@
   function loadAll()
   {
     global $mysqli;
-    
+
     $user = getSessionValue("user", array());
     $userId = $user[0]['user_id'];
-    //$userId = 3;
-    
+
     $response2 = array();
     $query2 = "select course_id from user_courses where user_id = ".$userId.";";
     $res = $mysqli->query($query2) or die(mysqli_error($mysqli));
@@ -54,19 +53,14 @@
         $response2[] = $row;
     }
 
-  //print_r($response2);
-  
     $response1 = array();
     $query1 = 'SELECT uc.taken, c1.course_id, c1.course_number, c1.course_name, group_concat(distinct pt.prereq_name ORDER BY pt.prereq_name SEPARATOR ", ") AS prerequisite, group_concat(distinct con.conc_name ORDER BY con.conc_name SEPARATOR ", ") AS concentration FROM courses c1 LEFT OUTER JOIN prereq_bridge pr on (pr.course_id = c1.course_id) LEFT OUTER JOIN prereq_text pt on (pr.prereq_id = pt.prereq_id) LEFT OUTER JOIN course_conc cc on (cc.course_id = c1.course_id) LEFT OUTER JOIN concentrations con on (cc.conc_id = con.conc_id) LEFT OUTER JOIN user_courses uc on (c1.course_id = uc.course_id) GROUP BY c1.course_number, c1.course_name;';
     $res = $mysqli->query($query1) or die(mysqli_error($mysqli));
     while($row = $res->fetch_assoc())
     {
         $row["taken"] = 0;
-        foreach ($response2 as $value) 
+        foreach ($response2 as $value)
         {
-  //print_r($value);
-  //print_r($row);
-  
           if ($value["course_id"] == $row["course_id"])
           {
             $row["taken"] = 1;
@@ -75,8 +69,6 @@
         }
         $response1[] = $row;
     }
-    
-    //setSessionValue("courses", $response);
     return $response1;
   }
 
