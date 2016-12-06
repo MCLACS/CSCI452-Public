0f4a27e23956af0fcda4633232634a6f91cf0153
Mark Cohen <mark.cohen@mcla.edu>
2016-12-06 14:42:20 +0000

    contentController.js added AJAX call to php

--- JavaScript/contentController.js
+++ JavaScript/contentController.js
@@ -7,7 +7,29 @@ function displayFields() {
   console.log(element);
 };
 
+function submitForm() {
+  var cmd = $("#action").val() + $("#type").val();
+  
+  var form = $('#contentInfo');
+  var formData = $(form).serialize();
+
+  $.ajax({
+    url: '/CSCI452-Public/PHP/createCourse.php?cmd=' + cmd,
+    type: 'GET',
+    contentType: "application/json",
+    data: formData,
+    success: function(json){
+      window.location.assign('/CSCI452-Public/Views/index.html');
+      console.log("success");
+    },
+    error:  function(request, status, error) {
+      alert(request.responseText);
+    }
+  });
+}
+
 function init() {
+  $('#submit-button').click(submitForm);
   $('#action').change(displayFields);
   $('#type').change(displayFields);
 };
